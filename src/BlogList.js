9a0cbda4072d52e8458a65ca9241fc5ab76b610b
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-28 15:34:41 +0530

    fillter data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,10 @@
-const BlogList = (props) =>{
-    const blogs =props.blogs;
+const BlogList = ({blogs, title}) =>{
+  
 
-    console.log(props,blogs);
-
+    
     return (
         <div className="blog-list">
+            <h2>{ title }</h2>
         {blogs.map(blog => (
          <div className="blog-preview" key={blog.id} >
             <h2>{ blog.title }</h2>
