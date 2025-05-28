0b70f8cfa0a61cd89e7bd3576bec0078b837fe30
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-28 15:51:01 +0530

    delete fonction

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,15 @@ const Home =() => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mahii', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs (newBlogs);
+  }
+     
    return (
    <div className="home">
-     < BlogList blogs ={blogs} title="All Blogs!"/>
-     < BlogList blogs ={blogs.filter((blog) => blog.author === 'mahii')} title="mahii's blog"/>
+     < BlogList blogs ={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+    
    </div>
   
    );
