fbcd19088659a1810c1918450807b489a61925e0
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-28 15:05:58 +0530

    debug

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,10 @@
-const BlogList = () =>{
+const BlogList = (props) =>{
+    const blogs =props.blogs;
+
+    console.log(props,blogs);
+
     return (
-        <div className="blog list">
+        <div className="blog-list">
         {blogs.map(blog => (
          <div className="blog-preview" key={blog.id} >
             <h2>{ blog.title }</h2>
