8df2cdc2109d33bc1a88d113b59676326dfc38c2
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-30 12:33:03 +0530

    loading time

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,27 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+  const[isPending , setIsPending] = useState(true);
 
-  const handleDelete = (id) => {
-  const newBlogs = blogs.filter (blog => blog.id !== id);
-  setBlogs(newBlogs);
-}
 
   useEffect(() => {
+    setTimeout(() =>{
     fetch('http://localhost:3005/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
          setBlogs(data);
-      })
+         setIsPending(false);
+      });
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
-      { blogs && <BlogList blogs={blogs} title ="All Blogs" handleDelete={handleDelete }/>}
+      {isPending && <div> Loading....... </div>}
+      { blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
