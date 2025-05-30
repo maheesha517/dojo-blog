9f394abe62dce3edafcca304526087f5fad7329e
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-30 13:35:30 +0530

    useFetch

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,16 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const[isPending , setIsPending] = useState(true);
-
-
-  useEffect(() => {
-    setTimeout(() =>{
-    fetch('http://localhost:3005/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-         setBlogs(data);
-         setIsPending(false);
-      });
-    }, 1000);
-  }, [])
+  const { data: blogs, isPending, error } = useFetch('http://localhost:3005/blogs');
 
   return (
     <div className="home">
-      {isPending && <div> Loading....... </div>}
-      { blogs && <BlogList blogs={blogs} />}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
     </div>
   );
 }
- 
+
 export default Home;
