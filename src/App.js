2d11f2ba1ed92f296192f3ef272fd159f792b473
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-30 13:45:11 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+ 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar/>
-
       <div className="content">
+      <Switch>
+        <Router path ="/">
         <Home/>
+        </Router>
+      </Switch>
       </div> 
     </div>
+    </Router>
   );
 }
 
