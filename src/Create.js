c0bcd9f095e9d7887c1e0e6b5feff00d7f567672
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-05-31 12:58:35 +0530

    Route pages

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,6 @@
 const Create = () => {
   return (
-    <div className="create">
+    <div className="Create">
       <h2>Add a New Blog</h2>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react";
-import BlogList from "./BlogList";
-import useFetch from "./useFetch";
+import useFetch from './useFetch';  
+import BlogList from './BlogList';
 
 const Home = () => {
-  const { data: blogs, isPending, error } = useFetch('http://localhost:3005/blogs');
+  const { error, isPending, data: blogs }  = useFetch('http://localhost:3005/blogs');
 
-   return (
+  return (
     <div className="home">
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs} /> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
     </div>
   );
 }
